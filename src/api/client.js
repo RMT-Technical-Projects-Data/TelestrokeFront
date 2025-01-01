f04c15aa2664a77ad94690b7ff8968418f434632
Istafa-Malik <istafamalik1996@gmail.com>
2025-01-01 19:15:36 +0500

    Commit 30 (Deployed)

--- src/api/client.js
+++ src/api/client.js
@@ -2,13 +2,6 @@ import axios from "axios";
 
 const client = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}`
-    // FOR LAPTOP AT HOME
-    // baseURL: 'http://192.168.100.157:8000/'
-    // baseURL: 'http://192.168.100.7:8000/'
-    // OSAMA'S MOB
-    // baseURL: 'http://192.168.252.146:8000/'
-    // baseURL: 'http://192.168.72.146:8000/'
-    // 192.168.252.146
-    
+ 
 })
 export default client;
